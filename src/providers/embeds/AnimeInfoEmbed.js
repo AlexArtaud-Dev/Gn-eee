5e8be65acf65bd0a75fbc8b52e300ed39a6a25de
AlexArtaud-Dev <smlartaudalexandre@gmail.com>
2022-08-05 18:54:15 +0200

    Fix anime info command for films

--- src/providers/embeds/AnimeInfoEmbed.js
+++ src/providers/embeds/AnimeInfoEmbed.js
@@ -8,7 +8,7 @@ class AnimeInfoEmbed extends AnimeEmbed{
             .setTimestamp()
 
         if (this.data !== null) {
-            let command = "/gogoanime stream name:" + this.data.id + " episode:<Episode Number>"
+            let command = "/gogoanime stream name:" + this.data.id + ` episode:${this.data.totalEpisodes > 1 ? "<Episode Number>": "1"}`;
             this.embed.setDescription(`**▸ Title :** ${this.data.title}\n**▸ Genres :** ${this.data.genres.join(", ")}\n**▸ Status:** ${this.data.status}\n**▸ Release Date:** ${this.data.releaseDate}\n**▸ Description:** \n${this.data.description}\n\n**▸ Last Episode Number:** ${this.data.totalEpisodes}\n----------------------\n**To get an episode copy paste this command:**\n\`\`\`${command}\`\`\``)
             this.embed.setThumbnail(this.data.image)
         }
